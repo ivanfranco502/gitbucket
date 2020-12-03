142d1463a60ce8189958a7b461e39c2e68ecdd61
Ivan Franco <ivan.franco@betssongroup.com>
2020-12-03 21:26:48 +0100

    Make more robust the mechanism to force the setting trough the pages  navigation flow. In case I can't update the url value cause the content script is executed before (or is an ajax response and is not even triggered), I'm forcing it with a redirect.

--- src/gitbucket.js
+++ src/gitbucket.js
@@ -4,11 +4,16 @@ chrome.storage.sync.get('hideWhitespace', function(data) {
     let hideWhitespace = data.hideWhitespace;
 
     if (hideWhitespace) {
+        let currentUrl = window.location.href;
+        if (currentUrl.match(prFileRegex) && currentUrl.indexOf('?w=') === -1 && currentUrl.indexOf('&w=') === -1) {
+                currentUrl.match(queryStringRegex) ? window.location.href += "&w=1" : window.location.href += "?w=1";
+        }
+
         let anchors = document.getElementsByTagName("a"); 
         for (let idx = 0; idx < anchors.length; ++idx) {
             if (anchors[idx].href.match(prFileRegex)) {
                 let compareFileUrl = anchors[idx].href;
-                if (compareFileUrl.indexOf('?w=') == -1 && compareFileUrl.indexOf('&w=') == -1) {
+                if (compareFileUrl.indexOf('?w=') === -1 && compareFileUrl.indexOf('&w=') === -1) {
                     compareFileUrl.match(queryStringRegex) ? anchors[idx].href += "&w=1" : anchors[idx].href += "?w=1"; 
                 }
             }
