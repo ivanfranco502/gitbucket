3b1071713498022b7ac83ccd575cca210310e199
Iván Franco <ivanfranco502@gmail.com>
2020-12-03 21:34:59 +0100

    Issue 2 avoid append query string twice (#3)
    * Google chrome initial setup
    
    * including images
    general logic to analyze anchors and update their href values.
    
    * Removing unused code and adding content script to perform update in the
    DOM. Also, handle the persistence of the configuration
    
    * Improving setting page style.
    
    * Fix #2: avoid append duplicate query string.
    Includes an additional validation to append the whitespace parameter
    properly if there is already another parameter.
    
    * Make more robust the mechanism to force the setting trough the pages
     navigation flow.
    In case I can't update the url value cause the content script is
    executed before (or is an ajax response and is not even triggered), I'm
    forcing it with a redirect.
    
    * bumping version

--- src/gitbucket.js
+++ src/gitbucket.js
@@ -1,12 +1,21 @@
 chrome.storage.sync.get('hideWhitespace', function(data) {
-    let regex = /([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?\/pull\/[0-9]+\/files/gi;
+    let prFileRegex = /([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?\/pull\/[0-9]+\/files/gi;
+    let queryStringRegex = /\?.+=.*/g;
     let hideWhitespace = data.hideWhitespace;
 
     if (hideWhitespace) {
+        let currentUrl = window.location.href;
+        if (currentUrl.match(prFileRegex) && currentUrl.indexOf('?w=') === -1 && currentUrl.indexOf('&w=') === -1) {
+                currentUrl.match(queryStringRegex) ? window.location.href += "&w=1" : window.location.href += "?w=1";
+        }
+
         let anchors = document.getElementsByTagName("a"); 
         for (let idx = 0; idx < anchors.length; ++idx) {
-            if (anchors[idx].href.match(regex)) {
-                anchors[idx].href += "?w=1"; 
+            if (anchors[idx].href.match(prFileRegex)) {
+                let compareFileUrl = anchors[idx].href;
+                if (compareFileUrl.indexOf('?w=') === -1 && compareFileUrl.indexOf('&w=') === -1) {
+                    compareFileUrl.match(queryStringRegex) ? anchors[idx].href += "&w=1" : anchors[idx].href += "?w=1"; 
+                }
             }
         }
     }
