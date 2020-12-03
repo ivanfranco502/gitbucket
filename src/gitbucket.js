c07d3964d8e1454460abacaa720344b7fcd8a08b
Ivan Franco <ivan.franco@betssongroup.com>
2020-12-03 19:38:05 +0100

    Fix #2: avoid append duplicate query string. Includes an additional validation to append the whitespace parameter properly if there is already another parameter.

--- src/gitbucket.js
+++ src/gitbucket.js
@@ -1,12 +1,16 @@
 chrome.storage.sync.get('hideWhitespace', function(data) {
-    let regex = /([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?\/pull\/[0-9]+\/files/gi;
+    let prFileRegex = /([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?\/pull\/[0-9]+\/files/gi;
+    let queryStringRegex = /\?.+=.*/g;
     let hideWhitespace = data.hideWhitespace;
 
     if (hideWhitespace) {
         let anchors = document.getElementsByTagName("a"); 
         for (let idx = 0; idx < anchors.length; ++idx) {
-            if (anchors[idx].href.match(regex)) {
-                anchors[idx].href += "?w=1"; 
+            if (anchors[idx].href.match(prFileRegex)) {
+                let compareFileUrl = anchors[idx].href;
+                if (compareFileUrl.indexOf('?w=') == -1 && compareFileUrl.indexOf('&w=') == -1) {
+                    compareFileUrl.match(queryStringRegex) ? anchors[idx].href += "&w=1" : anchors[idx].href += "?w=1"; 
+                }
             }
         }
     }
