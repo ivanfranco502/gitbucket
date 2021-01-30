6bcbb69804192800b74ed923baf197ee0f99f89d
Iván Franco <ivanfranco502@gmail.com>
2021-01-30 22:34:20 +0100

    Adding merge status page on top of PRs view (#4)

--- src/gitbucket.js
+++ src/gitbucket.js
@@ -20,4 +20,26 @@ chrome.storage.sync.get('hideWhitespace', function(data) {
         }
     }
 });
-  
+
+
+let conversationElement = document.getElementById("discussion_bucket");
+
+if (conversationElement != null) {
+    let discussionTimeline = conversationElement.getElementsByClassName("pull-discussion-timeline")[0];
+    let discussionMerging = document.getElementById("partial-pull-merging");
+    let cloneMergingDiv = discussionMerging.cloneNode(true);
+    let mergingDetails = cloneMergingDiv.getElementsByClassName("js-details-container")[0];
+
+    cloneMergingDiv.removeChild(cloneMergingDiv.getElementsByClassName("sr-only")[0]);
+
+    let removeP = mergingDetails.getElementsByTagName("p")[0];
+    if(removeP != null) {
+        mergingDetails.removeChild(removeP);
+    }
+    let mergeMessage = mergingDetails.getElementsByClassName("merge-message")[0];
+    if (mergeMessage != null){
+        mergeMessage.remove();
+    }
+    mergingDetails.classList.remove("merge-pr");
+    discussionTimeline.insertBefore(cloneMergingDiv, discussionTimeline.firstChild);
+}
